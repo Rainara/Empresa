0ac21ab0af3caf850860c8fe917e7bfb7a1a0de3
Rainara Araújo Mateus <rainaramateus@gmail.com>
2017-11-17 12:15:16 -0200

    change2

--- Empresa/Controllers/HomeController.cs
+++ Empresa/Controllers/HomeController.cs
@@ -36,6 +36,17 @@ namespace Empresa.Controllers
 
         }
 
+        public JsonResult GetEmployees(string search)
+        {
+            EmpresaContext db = new EmpresaContext();
+            IList<Employee> employees = db.employees.Where(x => x.Name.Contains(search)).Select(x => new Employee
+            {
+                Id = x.Id,
+                Name = x.Name
+            }).ToList();
+            return new JsonResult { Data = employees, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
 
     }
 }
